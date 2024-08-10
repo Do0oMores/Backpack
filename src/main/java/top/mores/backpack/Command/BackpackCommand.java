59eddb05595841f97277e4a56962595c70cf0a39
Do0oMores <do0omores@outlook.com>
2024-08-10 16:34:53 +0800

    RENEW 重载配置文件指令

--- src/main/java/top/mores/backpack/Command/BackpackCommand.java
+++ src/main/java/top/mores/backpack/Command/BackpackCommand.java
@@ -5,18 +5,25 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import top.mores.backpack.Backpack;
 import top.mores.backpack.GUI.MainGUI;
 
 public class BackpackCommand implements CommandExecutor {
     MainGUI mainGUI = new MainGUI();
 
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender,@NotNull Command command,@NotNull String s,@NotNull String[] strings) {
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (commandSender instanceof Player) {
             //打开背包
             if (strings.length == 1 && strings[0].equals("open")) {
                 Player sender = (Player) commandSender;
                 mainGUI.CreateMainInventory(sender);
+            } else if (strings.length == 1 && strings[0].equals("reload")) {
+                if (commandSender.isOp()) {
+                    Backpack.getInstance().reloadConfig();
+                    Backpack.getInstance().reloadData();
+                    commandSender.sendMessage("已重载配置文件");
+                }
             }
         } else {
             commandSender.sendMessage("只有玩家在游戏内可以这样做！");
