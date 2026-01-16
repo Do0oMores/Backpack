dc81fcb9299a5662472528dedb98ec4b6ee6c8b9
Do0oMores <do0omores@outlook.com>
2026-01-17 00:57:03 +0800

    RENEW：技能GUI

--- src/main/java/top/mores/backpack/GUI/SingleBackpack.java
+++ src/main/java/top/mores/backpack/GUI/SingleBackpack.java
@@ -1,21 +1,26 @@
 package top.mores.backpack.GUI;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import top.mores.backpack.Backpack;
 import top.mores.backpack.Utils.ItemStackUtil;
 import top.mores.backpack.Utils.MatchUtil;
 import top.mores.backpack.Utils.ArmorUtil;
+import top.mores.backpack.Utils.MessageUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class SingleBackpack {
     MatchUtil matchUtil = new MatchUtil();
+    MessageUtil messageUtil=new MessageUtil();
 
     /**
      * 单个背包物品数组
@@ -39,9 +44,36 @@ public class SingleBackpack {
      * @param slot   物品槽
      */
     public void CreateSingleInventory(Player player, int slot) {
-        Inventory singleInventory = Bukkit.createInventory(player, 9, "§a背包" + slot);
+        //背包格式：两行物品栏
+        Inventory singleInventory = Bukkit.createInventory(player, 18, "§a背包" + slot);
         for (ItemStack item : SingleBackpackItems(player.getName(), slot)) {
             singleInventory.setItem(singleInventory.firstEmpty(), item);
+        }
+        //第二行GUI
+        ItemStack barrier = createGUIItem(Material.BARRIER,
+                messageUtil.getOneItemName(),
+                messageUtil.getOneItemLore(), true);
+        ItemStack whitePane = createGUIItem(Material.WHITE_STAINED_GLASS_PANE,
+                messageUtil.getTwoItemName(),
+                messageUtil.getTwoItemLore(), false);
+        ItemStack redPane = createGUIItem(Material.RED_STAINED_GLASS_PANE,
+                messageUtil.getThreeItemName(),
+                messageUtil.getThreeItemLore(), true);
+
+        int[] barrierSlots = {9, 10, 11};
+        int[] whitePaneSlots = {12, 13, 14};
+        int[] redPaneSlots = {15, 16, 17};
+
+        for(int s:barrierSlots){
+            singleInventory.setItem(s,barrier.clone());
+        }
+
+        for(int s:whitePaneSlots){
+            singleInventory.setItem(s,whitePane.clone());
+        }
+
+        for (int s:redPaneSlots){
+            singleInventory.setItem(s,redPane.clone());
         }
         player.openInventory(singleInventory);
     }
@@ -95,5 +127,30 @@ public class SingleBackpack {
                 ArmorUtil.equipDefaultArmor(player);
             });
         });
+    }
+
+    //创建背包物品
+    private ItemStack createGUIItem(Material material,
+                                    String name,
+                                    String lore,
+                                    boolean unbreakable) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+            meta.setDisplayName(name);
+            if (lore != null && !lore.isEmpty()) {
+                meta.setLore(Arrays.asList(lore.split("\n")));
+            }
+
+            if (unbreakable) {
+                meta.setUnbreakable(true);
+                meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            }
+
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            item.setItemMeta(meta);
+        }
+        return item;
     }
 }
